39a9beed2acb9249a318e239e8cdeeff32897e15
Weiyu Zhao <weiyu.zhao.2023@gmail.com>
2024-03-05 15:12:49 -0800

    create home and about section

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+    strings: ["Full Stack Development", "Frontend Development", "Backend Development", "Web Designing", "Web Development"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 25,
+    backDelay: 500,
+});
